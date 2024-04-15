3879eb2fbd02872307b9340475172c129f124841
Tomas Balducchi <tomasbalducchi@gmail.com>
2024-04-15 16:35:55 -0300

    restyle

--- components/SliderData.ts
+++ components/SliderData.ts
@@ -1,40 +1,22 @@
-import Cel from '../public/assets/images/celulares.jpg';
-import Funda from '../public/assets/images/funda.jpg';
-import Vidrio from '../public/assets/images/vidrio.jpg';
-import Cargadores from '../public/assets/images/cargadores.jpg';
-import Auris from '../public/assets/images/auris.jpg';
-import Mouse from '../public/assets/images/mouse.jpg';
+import Cel from '../public/assets/images/celus.png';
+import Vidrio from '../public/assets/images/glass.png';
+import Cargadores from '../public/assets/images/cargadores.png';
+import Auris from '../public/assets/images/auriss.png';
+import Mouse from '../public/assets/images/mouse.png';
+import Fundas from '../public/assets/images/funds.png';
 
-// export const SliderData = [
-//     {
-//       image:
-//         'https://images.unsplash.com/photo-1535224206242-487f7090b5bb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
-//     },
-//     {
-//       image:
-//         'https://images.unsplash.com/photo-1469362102473-8622cfb973cd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2419&q=80',
-//     },
-//     {
-//       image:
-//         'https://images.unsplash.com/photo-1594717527389-a590b56e8d0a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
-//     },
-//     {
-//       image:
-//         'https://images.unsplash.com/photo-1484591974057-265bb767ef71?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
-//     },
-//     {
-//       image:
-//         'https://images.unsplash.com/photo-1545917992-dea2d782ef46?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2489&q=80',
-//     },
-//   ];
 export const SliderData = [
+  {
+    image:
+      Auris,
+  },
   {
     image:
       Cel,
   },
   {
     image:
-      Funda,
+      Mouse,
   },
   {
     image:
@@ -42,14 +24,10 @@ export const SliderData = [
   },
   {
     image:
-      Auris,
+      Fundas,
   },
   {
     image:
       Cargadores,
-  },
-  {
-    image:
-      Mouse,
   }
 ];
